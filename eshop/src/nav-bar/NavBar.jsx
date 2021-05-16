133bb481a84d158d0236f3a62c4b6b5787f4c7b7
agnejan <66556527+agnejan@users.noreply.github.com>
2021-05-16 21:13:20 +0200

    created NavItem and moved Navbar there

--- eshop/src/nav-bar/NavBar.jsx
+++ eshop/src/nav-bar/NavBar.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import NavItem from "./components/NavItem";
+
+const navItems = [
+  { text: "PRODUCTS", route: "/" },
+  { text: "VISION", route: "/vision" },
+];
 
 function NavBar() {
   return (
     <nav className="bg-red-500">
       <ul className="flex">
-        <li className="p-2 font-semibold text-xl hover:text-purple-700">
-          <Link to="/">PRODUCTS</Link>
-        </li>
-        <li className="p-2 font-semibold text-xl hover:text-purple-700">
-          <Link to="/vision">VISION</Link>
-        </li>
+        {navItems.map((x) => (
+          <NavItem {...x} />
+        ))}
       </ul>
     </nav>
   );
