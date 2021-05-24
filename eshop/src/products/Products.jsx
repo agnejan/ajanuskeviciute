3618892f290ce8774c8384f31a2d7e47af6a32ba
agnejan <66556527+agnejan@users.noreply.github.com>
2021-05-24 14:41:59 +0200

    show product images

--- eshop/src/products/Products.jsx
+++ eshop/src/products/Products.jsx
@@ -17,7 +17,14 @@ function Products() {
     <ul className="flex flex-wrap">
       {products.map((x) => (
         <li key={x.id} className="p-3">
-          <div className="bg-white border shadow-sm rounded w-64 h-80"></div>
+          <div className="bg-white border shadow-sm rounded w-64 h-80">
+            <div className="h-3/4">
+              <img src={x.image} alt={x.title} className="max-h-full m-auto" />
+            </div>
+            <div>Price</div>
+            <div>Title</div>
+            <div>Link</div>
+          </div>
         </li>
       ))}
     </ul>
