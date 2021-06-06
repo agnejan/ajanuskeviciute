a2b790d8dffc06a31f36b88f1eab975e80d85943
agnejan <66556527+agnejan@users.noreply.github.com>
2021-06-06 13:49:54 +0200

    call api to get single product info

--- eshop/src/product/Product.jsx
+++ eshop/src/product/Product.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function Product() {
   const { id } = useParams();
+  const [product, setProduct] = useState();
 
-  console.log("route params", id);
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const product = await response.json();
+      setProduct(product);
+    };
+
+    getProduct();
+  }, [id]);
+
   return <div>Product Page {id}</div>;
 }
 
