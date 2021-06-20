51bdda9848ab11e3ad3e370bf3cc857c418a8e10
agnejan <66556527+agnejan@users.noreply.github.com>
2021-06-20 11:03:13 +0200

    handle Counter edge cases

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -8,20 +8,26 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function handleIncrement() {
-    setCount((prevCount) => prevCount + 1);
+    if (count === '') {
+      setCount(1);
+    } else {
+      setCount((prevCount) => prevCount + 1);
+    }
   }
 
   function handleDecrement() {
-    setCount((prevCount) => prevCount - 1);
+    if (count === 1 || count === '') {
+      setCount(1);
+    } else {
+      setCount((prevCount) => prevCount - 1);
+    }
   }
 
   function handleInputChange(event) {
     const val = event.target.value;
-    if (!val) {
-      setCount(1);
-    } else {
-      setCount(parseInt(val));
-    }
+    const newVal = val === '' ? '' : parseInt(val);
+
+    setCount(newVal);
   }
 
   return (
