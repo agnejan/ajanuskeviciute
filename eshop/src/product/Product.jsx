333591afd80c256011f2e2b27b01579a47c2f236
agnejan <66556527+agnejan@users.noreply.github.com>
2021-06-20 19:55:08 +0200

    use counter hook logic in Product.jsx

--- eshop/src/product/Product.jsx
+++ eshop/src/product/Product.jsx
@@ -2,15 +2,17 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Card from '../common/components/Card';
-import { getProduct } from '../common/requests';
+import { addToCart, getProduct } from '../common/requests';
 import Counter from '../common/components/Counter';
 import Button from '../common/components/Button';
 import Spinner from '../common/components/Spinner';
+import useCounter from '../common/hooks/useCounter';
 
 function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const counterProps = useCounter();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -22,6 +24,11 @@ function Product() {
 
     fetchProduct();
   }, [id]);
+
+  function handleAddToCartClick() {
+    console.log('addtocart', counterProps.count);
+    axios(addToCart());
+  }
 
   if (isLoading) {
     return <Spinner text='Fetching product info' />;
@@ -39,8 +46,10 @@ function Product() {
         <div className='text-3xl mb-4 font-bold'>$ {product?.price}</div>
         <p className='mb-4'>{product?.description}</p>
         <div className='mb-1 font-semibold'>Quantity:</div>
-        <Counter className='mb-4' />
-        <Button type='primary'>ADD TO CART</Button>
+        <Counter className='mb-4' {...counterProps} />
+        <Button type='primary' onClick={handleAddToCartClick}>
+          ADD TO CART
+        </Button>
       </div>
     </div>
   );
