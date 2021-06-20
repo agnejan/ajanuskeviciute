b78e4c7ddede59a4d4e92af222376b8f5425d56f
agnejan <66556527+agnejan@users.noreply.github.com>
2021-06-20 12:32:53 +0200

    added loading state to Product and Products componenets

--- eshop/src/product/Product.jsx
+++ eshop/src/product/Product.jsx
@@ -1,37 +1,43 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import Card from "../common/components/Card";
-import { getProduct } from "../common/requests";
-import Counter from "../common/components/Counter";
-import Button from "../common/components/Button";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import Card from '../common/components/Card';
+import { getProduct } from '../common/requests';
+import Counter from '../common/components/Counter';
+import Button from '../common/components/Button';
 
 function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchProduct = async () => {
+      setIsLoading(true);
       const { data } = await axios(getProduct(id));
-
+      setIsLoading(false);
       setProduct(data);
     };
 
     fetchProduct();
   }, [id]);
 
+  if (isLoading) {
+    return <div>LOADING</div>;
+  }
+
   return (
-    <div className="flex m-auto px-4" style={{ maxWidth: 1000 }}>
-      <div className="pr-3 flex-1 max-w-1/2">
+    <div className='flex m-auto px-4' style={{ maxWidth: 1000 }}>
+      <div className='pr-3 flex-1 max-w-1/2'>
         <Card>
           <img src={product?.image} alt={product?.title} />
         </Card>
       </div>
-      <div className="pl-3 max-w-1/2">
-        <h1 className="text-2xl mb-4 font-semibold">{product?.title}</h1>
-        <div className="text-3xl mb-4 font-bold">{product?.price}$</div>
-        <p className="mb-4">{product?.description}</p>
-        <div className="mb-1 font-semibold">Quantity:</div>
+      <div className='pl-3 max-w-1/2'>
+        <h1 className='text-2xl mb-4 font-semibold'>{product?.title}</h1>
+        <div className='text-3xl mb-4 font-bold'>{product?.price}$</div>
+        <p className='mb-4'>{product?.description}</p>
+        <div className='mb-1 font-semibold'>Quantity:</div>
         <Counter />
         <Button>ADD TO CART</Button>
       </div>
--- eshop/src/products/Products.jsx
+++ eshop/src/products/Products.jsx
@@ -1,22 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { getProducts } from "../common/requests";
-import ProductCard from "./components/ProductCard";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { getProducts } from '../common/requests';
+import ProductCard from './components/ProductCard';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
+      setIsLoading(true);
       const { data } = await axios(getProducts);
+      setIsLoading(false);
       setProducts(data);
     };
 
     fetchProducts();
   }, []);
 
+  if (isLoading) {
+    return <div>LOADING</div>;
+  }
+
   return (
-    <ul className="flex flex-wrap">
+    <ul className='flex flex-wrap'>
       {products.map((x) => (
         <ProductCard
           key={x.id}
