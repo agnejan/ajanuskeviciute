cc635e84c8779f4049d289945ecacd09fdb677cd
agnejan <66556527+agnejan@users.noreply.github.com>
2021-06-20 23:14:29 +0200

    changed Buttonspinner colour

--- eshop/src/common/components/ButtonSpinner.jsx
+++ eshop/src/common/components/ButtonSpinner.jsx
@@ -3,7 +3,7 @@ import Loader from 'react-loader-spinner';
 function ButtonSpinner() {
   return (
     <div className='flex'>
-      <Loader type='ThreeDots' color='#EF4444' height={22} width={100} />
+      <Loader type='ThreeDots' color='#34D399' height={22} width={100} />
     </div>
   );
 }
